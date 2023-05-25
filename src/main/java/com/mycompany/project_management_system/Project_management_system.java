e13141d5972487e76789560254a5abc36b50543c
unknown <badalsinghania123@gmail.com>
2023-05-25 12:05:01 +0530

    initiate commit

---
+++ src/main/java/com/mycompany/project_management_system/Project_management_system.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.project_management_system;
+
+/**
+ *
+ * @author BADAL
+ */
+public class Project_management_system {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
